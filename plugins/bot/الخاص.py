307ed1d6e09820b730e580ba60695a8dadb673a9
ahmedayyad <ahmed@ahmedayyad.dev>
2026-08-27 19:17:40 +0300

    Revert "Initial commit"
    This reverts commit bcaf45f879e83f493c62ea067b9bb55b744b3e8c.

--- plugins/bot/الخاص.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-
-from ahmedyad.config import user
-
-
-@Client.on_message(filters.private)
-async def hello(client, message):
-    await message.reply(f"عذرا هذا البوت خاص بحساب المطور {user.me.username} ولم يفيدك في اي شيئ")
